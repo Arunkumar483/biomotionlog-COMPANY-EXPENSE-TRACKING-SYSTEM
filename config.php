97b4d95547c0d780003908b9d5df784a13d56469
Arunkumar <52123143+Arunkumar483@users.noreply.github.com>
2021-02-06 01:29:37 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+	// Database credentials
+	define('DB_SERVER', 'localhost');
+	define('DB_USERNAME', 'id16053710_arunkumar');
+	define('DB_PASSWORD', '<77\Qe$zes8A2KT=');
+	define('DB_NAME', 'id16053710_login_system');
+
+	// Attempt to connect to MySQL database
+	$mysql_db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+	if (!$mysql_db) {
+		die("Error: Unable to connect " . $mysql_db->connect_error);
+	}
