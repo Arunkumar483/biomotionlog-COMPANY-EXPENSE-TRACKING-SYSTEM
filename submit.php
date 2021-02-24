22fc429712bd4d69877618340d258fe3288d1ecf
Arunkumar <52123143+Arunkumar483@users.noreply.github.com>
2021-02-24 10:27:36 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,44 +1,80 @@
+<?php
+	// Initialize session
+	session_start();
+
+	if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] !== false) {
+		header('location: index.php');
+		exit;
+	}
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title>Add Records in Database</title>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/lux/bootstrap.min.css" integrity="sha384-9+PGKSqjRdkeAU7Eu4nkJU8RFaH8ace8HGXnkiKMP9I9Te0GJ4/km3L1Z8tXigpG" crossorigin="anonymous">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+       
+    </style>
 </head>
 <body>
-
-<?php
- require_once "config/config.php"; // Using database connection file here
-
-if(isset($_POST['submit']))
-{		
-    $pname = $_POST['pname'];
-    $amount = $_POST['amount'];
-
-    $insert = mysqli_query($mysql_db,"INSERT INTO `prexp`(`pname`, `amount`) VALUES ('$pname','$amount')");
-
-    if(!$insert)
-    {
-        echo mysqli_error();
-    }
-    else
-    {
-        echo "Records added successfully.";
-    }
-}
-
-mysqli_close($mysql_db); // Close connection
-?>
-
-<h3>Fill the Form</h3>
-
-<form method="POST">
-  pname : <input type="text" name="pname" placeholder="Enter pname" Required>
-  <br/>
-  amount : <input type="text" name="amount" placeholder="Enter amount" Required>
-  <br/>
-  <input type="submit" name="submit" value="Submit">
-  <a href="logout.php" class="btn btn-block btn-outline-danger">Sign Out</a>
-</form>
-
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Expenses/Advances Entry Form</h2>
+                    </div>
+                    <p>Please fill this form and submit to add EXP/ADV record to the database.</p>
+                    <form action="insert.php" method="post">
+                        <div class="form-group">
+                            <label>Project id</label>
+                            <input type="number" name="pid" class="form-control">
+                        </div>
+                        <div class="form-group" >
+                            <label>TYPE</label>
+                            <br>
+                            <select name="type" id="type"  style="width:500px;">
+                                <option value="Project Expense">Project Expense</option>
+                                <option value="Project Advance">Project Advance</option>
+                                <option value="Tour Expense">Tour Expense</option>
+                                <option value="Tour Advance">Tour Advance</option></option>
+                                                    
+                            </select>
+                        </div>
+                        <div class="form-group" >
+                            <label>ENTRY TYPE</label>
+                            <br>
+                            <select name="entrytype" id="entrytype"  style="width:500px;">
+                                <option value="Travel">Travel</option>
+                                <option value="Installation">Installation</option>
+                                <option value="Food">Food</option>
+                                <option value="Loading/Unloading">Loading/Unloading</option>
+                                <option value="Cleaning">Cleaning</option>
+                                <option value="Other Expense">Other Expense</option></option>
+                                                    
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Amount</label>
+                            <input type="number" name="amount" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Description</label>
+                            <input type="text" name="description" class="form-control">
+                        </div>
+                        
+                        <input type="submit" class="btn btn-primary" name="submit" value="Submit">
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
 </body>
 </html>
+
 
