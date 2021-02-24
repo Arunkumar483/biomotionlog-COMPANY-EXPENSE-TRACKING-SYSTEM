99e6e2442e4366b9a030638754975997342e0c42
Arunkumar <52123143+Arunkumar483@users.noreply.github.com>
2021-02-24 10:29:46 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -10,40 +10,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Welcome</title>
-	<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/cosmo/bootstrap.min.css" rel="stylesheet" integrity="sha384-qdQEsAI45WFCO5QwXBelBe1rR9Nwiss4rGEqiszC+9olH1ScrLrMQr1KmDR964uZ" crossorigin="anonymous">
+	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Welcome</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/lux/bootstrap.min.css" integrity="sha384-9+PGKSqjRdkeAU7Eu4nkJU8RFaH8ace8HGXnkiKMP9I9Te0GJ4/km3L1Z8tXigpG" crossorigin="anonymous">
 	<style>
         .wrapper{ 
         	width: 500px; 
-        	padding: 20px; 
+        	padding: 100px; 
         }
         .wrapper h2 {text-align: center}
-        .wrapper form .form-group span {color: red;}
+        .wrapper form .form-group span {color: blue;}
+        
 	</style>
-	<!DOCTYPE html> 
-<html> 
-<body> 
-
-<form align=center> 
-<a href="submit.php" class="btn btn-block btn-outline-danger">ENTER VALUES</a>
-
-</form> 
-
-</body> 
-</html> 
-
 </head>
 <body>
+    
 	<main>
 		<section class="container wrapper">
 			<div class="page-header">
-				<h2 class="display-5">Welcome home <?php echo $_SESSION['username']; ?></h2>
+				<h4 class="display-5">Welcome home <h3><?php echo $_SESSION['username']; ?></h3></h4>
 			</div>
 
 			<a href="password_reset.php" class="btn btn-block btn-outline-warning">Reset Password</a>
+			<a href="submit.php" class="btn btn-block btn-outline-danger">ADD DATA ENTRY</a>
 			<a href="logout.php" class="btn btn-block btn-outline-danger">Sign Out</a>
+			<a href="query123.php" class="btn btn-block btn-outline-danger">Other queries</a>
 		</section>
 	</main>
+	
 </body>
 </html>
