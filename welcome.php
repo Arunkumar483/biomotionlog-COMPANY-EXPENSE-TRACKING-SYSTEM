d137334d13067228e16e6003ef72b3ecda101529
Arunkumar <52123143+Arunkumar483@users.noreply.github.com>
2021-03-16 20:25:33 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -14,27 +14,43 @@
 	<title>Welcome</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/lux/bootstrap.min.css" integrity="sha384-9+PGKSqjRdkeAU7Eu4nkJU8RFaH8ace8HGXnkiKMP9I9Te0GJ4/km3L1Z8tXigpG" crossorigin="anonymous">
 	<style>
         .wrapper{ 
-        	width: 500px; 
-        	padding: 100px; 
+        	width: 400px; 
+        	padding: 50px; 
         }
         .wrapper h2 {text-align: center}
         .wrapper form .form-group span {color: blue;}
+        .di{
+	text-align:center;
+	font-size:25px;
+}
+.cont{
+	border:1px solid black;
+	box-shadow: 0px 1px 2px 1px;
+	margin-top:20px;
+	width:80vw;
+	background:white;
+}body{
+  background:#b2d4d9;
+}
         
 	</style>
 </head>
 <body>
     
 	<main>
-		<section class="container wrapper">
+		<section class="container wrapper cont">
+			<div class="di"><strong>User Dash Board</strong></div><hr>
 			<div class="page-header">
-				<h4 class="display-5">Welcome home <h3><?php echo $_SESSION['username']; ?></h3></h4>
+				<h4 class="display-5">Welcome <h3><?php echo $_SESSION['username']; ?></h3></h4>
 			</div>
-
-			<a href="password_reset.php" class="btn btn-block btn-outline-warning">Reset Password</a>
 			<a href="submit.php" class="btn btn-block btn-outline-danger">ADD DATA ENTRY</a>
-			<a href="logout.php" class="btn btn-block btn-outline-danger">Sign Out</a>
-			<a href="query123.php" class="btn btn-block btn-outline-danger">Other queries</a>
 			<a href="fetchdisplay.php" class="btn btn-block btn-outline-danger">VIEW MY ENTRIES</a>
+			<a href="query123.php" class="btn btn-block btn-outline-danger">Other queries</a>
+			<a href="password_reset.php" class="btn btn-block btn-outline-danger">Reset Password</a>
+			<a href="logout.php" class="btn btn-block btn-outline-danger">Sign Out</a>
+			<br>
+			<br>
+			
 		</section>
 	</main>
 	
