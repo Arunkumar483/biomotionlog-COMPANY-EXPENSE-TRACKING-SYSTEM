1f0b0dca92be30bde0df3a8735edfca058418d35
Arunkumar <52123143+Arunkumar483@users.noreply.github.com>
2021-03-16 20:20:12 +0530

    Update password_reset.php

--- password_reset.php
+++ password_reset.php
@@ -84,27 +84,42 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         }
         .wrapper h2 {text-align: center}
         .wrapper form .form-group span {color: red;}
+        .cont{
+	border:1px solid black;
+	box-shadow: 0px 1px 2px 1px;
+	margin-top:20px;
+	width:80vw;
+	margin:auto;
+	margin-top:7vh;
+	background:white;
+}
+body{
+	background:#b2d4d9;
+}
+.butt{
+    width:25vw;
+}
     </style>
 </head>
 <body>
-    <main class="container wrapper">
+    <main class="container wrapper cont">
         <section>
-            <h2>Reset Password</h2>
-            <p>Please fill out this form to reset your password.</p>
+            <h2><strong>Reset Password</strong></h2>
+            <center><p>Please fill out this form to reset your password.</p></center>
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
                 <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
-                    <label>New Password</label>
+                    <label><b>New Passwor</b></label>
                     <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
                     <span class="help-block"><?php echo $new_password_err; ?></span>
                 </div>
                 <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                    <label>Confirm Password</label>
+                    <label><b>Confirm Password</b></label>
                     <input type="password" name="confirm_password" class="form-control">
                     <span class="help-block"><?php echo $confirm_password_err; ?></span>
                 </div>
                 <div class="form-group">
-                    <input type="submit" class="btn btn-block btn-primary" value="Submit">
-                    <a class="btn btn-block btn-link bg-light" href="welcome.php">Cancel</a>
+                    <center><input type="submit" class="btn btn-primary butt" value="Submit"></center><br>
+                    <center><a class="btn btn-block butt" href="welcome.php">Cancel</a></center>
                 </div>
             </form>
         </section>
