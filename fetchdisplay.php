3b6cc3b02d1549e9f37c5f57b0d7d2af0ef5a5cf
Arunkumar <52123143+Arunkumar483@users.noreply.github.com>
2021-03-28 23:29:27 +0530

    Update fetchdisplay.php

--- fetchdisplay.php
+++ fetchdisplay.php
@@ -23,7 +23,7 @@ if (mysqli_num_rows($result) > 0) {
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=0.5">
 <title>Biomotion entries</title>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
