991d74211222ad5d28b807fd2f1fda36425c442b
Arunkumar <52123143+Arunkumar483@users.noreply.github.com>
2021-03-28 23:36:35 +0530

    Create searchinput.php

---
+++ searchinput.php
@@ -0,0 +1,7 @@
+<div class="test1-member" style="clear:both;">
+	<div class="col-lg-auto">
+		<input type="checkbox" name="check[]"/>
+		&nbsp;&nbsp;
+		<input type="text" autofocus="autofocus" name="member[]" size=75/>
+	</div>
+</div>
