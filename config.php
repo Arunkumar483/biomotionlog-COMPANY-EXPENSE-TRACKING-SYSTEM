c3149cf98605d12125bf985887e3545238550c19
Arunkumar <52123143+Arunkumar483@users.noreply.github.com>
2021-04-08 10:34:25 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,6 @@
 <?php
 	// Database credentials
-	define('DB_SERVER', 'localhost');
-	define('DB_USERNAME', 'id16053710_arunkumar');
-	define('DB_PASSWORD', 'Arundbbiomotionllog123*');
-	define('DB_NAME', 'id16053710_login_system');
+	
 
 	// Attempt to connect to MySQL database
 	$mysql_db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
