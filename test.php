e0af7b770f6b27bd5b062561bc29280275f6bd78
Arunkumar <52123143+Arunkumar483@users.noreply.github.com>
2021-05-09 15:57:11 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,8 @@
 <?php
 	// Initialize session
 	session_start();
-
 	if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] !== false) {
-		header('location: index.php');
-		echo "<h2>Hello we cannot recognize u kindly go to login page and login again<br></h2>";
+		header('location: adminlogin.php');
 		exit;
 	}
 ?>
@@ -15,23 +13,23 @@
 	<title>Welcome</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/lux/bootstrap.min.css" integrity="sha384-9+PGKSqjRdkeAU7Eu4nkJU8RFaH8ace8HGXnkiKMP9I9Te0GJ4/km3L1Z8tXigpG" crossorigin="anonymous">
 	<style>
         .wrapper{ 
-        	width: 400px; 
-        	padding: 50px; 
+        	width: 500px; 
+        	padding: 100px; 
+        	padding: 40px; 
         }
         .wrapper h2 {text-align: center}
         .wrapper form .form-group span {color: blue;}
-        .di{
-	text-align:center;
-	font-size:25px;
-}
-.cont{
+        .cont{
 	border:1px solid black;
 	box-shadow: 0px 1px 2px 1px;
 	margin-top:20px;
 	width:80vw;
+	margin:auto;
+	margin-top:7vh;
 	background:white;
-}body{
-  background:#b2d4d9;
+}
+body{
+	background:#b2d4d9;
 }
         
 	</style>
@@ -40,22 +38,23 @@
     
 	<main>
 		<section class="container wrapper cont">
-			<div class="di"><strong>User Dash Board</strong></div><hr>
 			<div class="page-header">
-				<h4 class="display-5">Welcome <h3><?php echo $_SESSION['username']; ?></h3></h4>
+				<h4 class="display-5">Welcome home admin <h3><?php echo $_SESSION['adminname']; ?></h3></h4>
 			</div>
-			<a href="submit.php" class="btn btn-block btn-outline-danger">ADD DATA ENTRY</a>
-			<a href="fetchdisplay.php" class="btn btn-block btn-outline-danger">VIEW MY ENTRIES</a>
-			<a href="index2.php" class="btn btn-block btn-outline-danger">QUERY BOX</a>
-			<a href="search.php" class="btn btn-block btn-outline-danger">REFER PROJECT DETAILS</a>
-			<a href="password_reset.php" class="btn btn-block btn-outline-danger">Reset Password</a>
-			<a href="logout.php" class="btn btn-block btn-outline-danger">Sign Out</a>	
-			
-			<br>
-			<br>
-			
+
+			<a href="adminpassreset.php" class="btn btn-block btn-outline-danger">Reset Password</a>
+
+			<a href="register.php" class="btn btn-block btn-outline-danger">ADD USER</a>
+			<a href="adminsignup.php" class="btn btn-block btn-outline-danger">ADD ADMIN</a>
+			<a href="filter.php" class="btn btn-block btn-outline-danger">Manage Entry</a>
+			<a href="newadmin.php" class="btn btn-block btn-outline-danger">Manage Project</a>
+			<a href="index2.php" class="btn btn-block btn-outline-danger">Query/Chatbox</a>
+			<a href="adminpassreset.php" class="btn btn-block btn-outline-danger">Reset Password</a>
+			<a href="adminlogout.php" class="btn btn-block btn-outline-danger">Sign Out</a>
+
+
 		</section>
 	</main>
-	
+
 </body>
 </html>
